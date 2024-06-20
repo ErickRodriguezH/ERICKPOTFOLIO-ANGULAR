0dfb83b6f43d5d94ba4e2147e92516e2fef355d1
ErickRodriguezH <erickrock1402@gmail.com>
2024-06-20 17:00:13 -0600

    add new components (home, about, contact, experinece, skills)

--- my-portfolio/src/main.ts
+++ my-portfolio/src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import { AboutComponent } from './app/about/about.component';
+import { ContactComponent } from './app/contact/contact.component';
+import { ExperienceComponent } from './app/experience/experience.component';
+import { HomeComponent } from './app/home/home.component';
+import { SkillsComponent } from './app/skills/skills.component';
+
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
